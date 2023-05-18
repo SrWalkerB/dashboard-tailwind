ccb09b2b3e03108cff4803720ba50129b1ce7a1e
SrWalkerB <srwalkerbdevs@gmail.com>
2023-05-18 16:48:55 -0300

    ajustando sidebar e menu

--- src/components/Menu/MenuComponent.tsx
+++ src/components/Menu/MenuComponent.tsx
@@ -2,32 +2,34 @@ import { Album, List } from "lucide-react"
 
 export default function Menu(){
     return (
-        <div className="bg-white w-full rounded-md p-3 flex flex-row items-center justify-between">
-            <div>
-                <input 
-                    className="rounded-xl p-2 w-96 border border-gray-300"
-                    placeholder="Search"
-                />
+        <div className="pr-10 pt-3">
+            <div className="bg-white w-full rounded-md p-3 flex flex-row items-center justify-between">
+                <div>
+                    <input 
+                        className="rounded-xl p-2 w-96 border border-gray-300"
+                        placeholder="Search"
+                    />
 
-                <input 
-                    className="rounded-xl p-2 w-96 ms-4  border border-gray-300"
-                    placeholder="Monday"
-                />
-            </div>
-
-            <div className="flex">
-                <div className="flex bg-black p-2 rounded-xl mr-7 items-center">
-                    <Album color="white" />
-                    <p className="text-cyan-50 ml-3 text-sm">
-                        Cards
-                    </p>
+                    <input 
+                        className="rounded-xl p-2 w-96 ms-4  border border-gray-300"
+                        placeholder="Monday"
+                    />
                 </div>
 
-                <div className="flex bg-white p-2 rounded-xl items-center">
-                    <List />
-                    <p className="text-black ml-3 text-sm">
-                        List
-                    </p>
+                <div className="flex">
+                    <div className="flex bg-black p-2 rounded-xl mr-7 items-center">
+                        <Album color="white" />
+                        <p className="text-cyan-50 ml-3 text-sm">
+                            Cards
+                        </p>
+                    </div>
+
+                    <div className="flex bg-white p-2 rounded-xl items-center">
+                        <List />
+                        <p className="text-black ml-3 text-sm">
+                            List
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/SideBar/SideBarComponent.tsx
+++ src/components/SideBar/SideBarComponent.tsx
@@ -2,21 +2,23 @@ import { SideBarItem } from "./SideBarItemComponent"
 
 export default function SideBar(){
     return(
-        <div className="bg-slate-300 w-64 h-full rounded-lg ms-4">
-            <div className="p-6 text-start border-b-2 border-gray-500">
-                <h2 className="font-bold text-xl">SrWalkerB</h2>
-            </div>
+        <div className="pt-3">
+            <div className="bg-slate-300 w-64 h-full rounded-lg ms-4">
+                <div className="p-6 text-start border-b-2 border-gray-500">
+                    <h2 className="font-bold text-xl">SrWalkerB</h2>
+                </div>
 
-            <nav className="mt-2 p-1">
-                <ul>
-                    <SideBarItem title="Dashboard" />
-                    <SideBarItem title="Projects" />
-                    <SideBarItem title="Task List" />
-                    <SideBarItem title="Services" />
-                    <SideBarItem title="Notifications" />
-                    <SideBarItem title="Chart" />
-                </ul>
-            </nav>
-      </div>
+                <nav className="mt-2 p-1">
+                    <ul>
+                        <SideBarItem title="Dashboard" />
+                        <SideBarItem title="Projects" />
+                        <SideBarItem title="Task List" />
+                        <SideBarItem title="Services" />
+                        <SideBarItem title="Notifications" />
+                        <SideBarItem title="Chart" />
+                    </ul>
+                </nav>
+            </div>
+        </div>
     )
 }
